65d1c676c648c53934006a8174741461e923e2b1
Joao Victor <joaovictordeveloper@outlook.com>
2023-02-10 23:11:20 -0300

    feat: importando infoBar e Header na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,52 +1,17 @@
 import React from "react";
-import {
-  Container,
-  Content,
-  Title,
-  SubTitle,
-  InfoBarContainer,
-  InfoBarContent,
-  ContactDiv,
-  InfoContact,
-  SocialDiv,
-  InfoSocial,
-} from "./styles";
 
-import {
-  AiFillMail,
-  AiFillPhone,
-  AiFillFacebook,
-  AiFillInstagram,
-} from "react-icons/ai";
+import Header from "../../components/Header";
+import InfoBar from "../../components/InfoBar";
+
+import { Container, Content } from "./styles";
 
 export default function Home() {
   return (
     <Container>
-      <InfoBarContainer>
-        <InfoBarContent>
-          <ContactDiv>
-            <InfoContact>
-              <AiFillMail color="#5183a6" />
-              unimagem@unimagempicos.com.br
-            </InfoContact>
-            <InfoContact>
-              <AiFillPhone color="#5183a6" />
-              (89) 3422-1463
-            </InfoContact>
-          </ContactDiv>
-          <SocialDiv>
-            <InfoSocial>
-              <AiFillFacebook color="#5183a6" />
-            </InfoSocial>
-            <InfoSocial>
-              <AiFillInstagram color="#5183a6" />
-            </InfoSocial>
-          </SocialDiv>
-        </InfoBarContent>
-      </InfoBarContainer>
+      <InfoBar/>
+
       <Content>
-        <Title>Hello World</Title>
-        <SubTitle>Junior Cintra</SubTitle>
+        <Header/>
       </Content>
     </Container>
   );
